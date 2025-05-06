6a6e5ee92ac492155ae576657e253162ef554987
ValentinAcieff <a.acieff@alumno.etec.um.edu.ar>
2025-05-06 08:23:53 -0300

    Etapa 2: Cálculo de Valores de la Función

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -8,5 +8,12 @@ fun main() {
         println("El valor de a no puede ser cero")
     }   
     println("La función cuadrática es: ${a}x² + ${b}x + ${c}")
-    
+    calculadora(a, b, c)
+}
+
+fun calculadora (a: Int, b: Int, c: Int){
+    for (x in -5..5) {
+        var resultado=a*x*x+b*x+c
+        println("Para X = ${x}, el resultado de la función cuadrática es ${resultado}")
+    }
 }
