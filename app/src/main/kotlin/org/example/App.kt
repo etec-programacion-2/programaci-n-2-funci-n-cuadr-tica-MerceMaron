029197d411fbd6a74549d53dc04e82933b516a15
ValentinAcieff <a.acieff@alumno.etec.um.edu.ar>
2025-05-06 08:21:56 -0300

    Etapa 1: Estructura Básica y Definición de Coeficientes

---
+++ app/src/main/kotlin/org/example/App.kt
@@ -0,0 +1,12 @@
+package org.example
+
+fun main() {
+    val a=5
+    val b=7
+    val c=3
+    if (a === 0) {
+        println("El valor de a no puede ser cero")
+    }   
+    println("La función cuadrática es: ${a}x² + ${b}x + ${c}")
+    
+}
